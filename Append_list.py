26790d515c4b5a3c246235a29dbbecc138ddb253
Ayan Sayyad <98633937+Ayan02-coder@users.noreply.github.com>
2024-09-03 16:47:10 +0530

    Create Append_list.py

---
+++ Append_list.py
@@ -0,0 +1,11 @@
+# WAP to ask the user to enter names of their 3 favorite movies & store them in a list
+movie = input("Enter a Movie name(3):").split()
+list = []   #list = [movie]
+list.append(movie)
+print(list)
+
+list =[]
+list.append(input("enter 1 movie name :"))
+list.append(input("enter 2 movie name :"))
+list.append(input("enter 3 movie name :"))
+print(list)
