5dbbd1f0fc664483ee1e5909e8664b8ad8c53316
Ayan Sayyad <98633937+Ayan02-coder@users.noreply.github.com>
2024-09-04 19:39:32 +0530

    Create 14_save_value_in_set_9_and_9.0.py

---
+++ 14_save_value_in_set_9_and_9.0.py
@@ -0,0 +1,12 @@
+# figure out a way to store 9 & 9.0 as separate values in the set.
+set = {9,"9.0"}
+print(set)
+
+#use built-in data type
+
+set1 ={
+    ("float", 9.0),
+    ("int", 9)
+
+}
+print(set1)
