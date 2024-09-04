cd5cb959b09bed9f088effae7f0ff88e9a48ed00
Ayan Sayyad <98633937+Ayan02-coder@users.noreply.github.com>
2024-09-04 19:17:00 +0530

    Create 10_set.py

---
+++ 10_set.py
@@ -0,0 +1,30 @@
+# collection = {1,2,3,"hello",7,6,6} 
+# print(collection)
+# print(type(collection))
+
+# collection1 = set() #empty set
+# print(collection1)
+
+# # methods
+# collection.add(10)
+# collection.add(9)
+# collection.add("ayan")
+# print(collection)
+
+# collection.remove(9)
+# print(collection)
+
+# collection.pop()
+# print(collection)
+
+# print(len(collection))
+# collection.clear()
+# print(collection)
+# print(len(collection))
+
+
+set1 = {1,2,3}
+set2 ={2,3,4}
+
+print(set1.union(set2))
+print(set1.intersection(set2))
