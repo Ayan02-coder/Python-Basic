22b531e66ad6b8842849d348fec14cc4b8927264
Ayan Sayyad <98633937+Ayan02-coder@users.noreply.github.com>
2024-09-04 19:18:11 +0530

    Create 12_classroom.py

---
+++ 12_classroom.py
@@ -0,0 +1,6 @@
+# you are given a list for subject for students. Assume one classrooom is required for 1 subject. how many classrooms are needed by students
+list = ["python","java","c++","python","javascript","java","python","java","c++","c"]
+
+a = set(list)
+
+print("Number of classes required are: ",len(a))
