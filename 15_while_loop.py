a753ab665fe6c0228a1c5458cf884db65c69001a
Ayan Sayyad <98633937+Ayan02-coder@users.noreply.github.com>
2024-09-05 16:46:30 +0530

    Update and rename while_loop.py to 15_while_loop.py

--- 15_while_loop.py
+++ 15_while_loop.py
@@ -44,7 +44,15 @@ while i < len(list):
     i += 1
 
 
-
+#WAP to find the sum of first n numbers
+n = int(input("Enter a number: "))
+sum =0
+i = 1
+while i <=n:
+    sum += i
+    i += 1
+    
+print(sum)
 
 
 
